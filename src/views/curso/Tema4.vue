e67fc840a1158179f0b12426c78c831e7a72ef16
Camilo <camilobr17@gmail.com>
2024-05-28 07:04:25 -0500

    avances_tema4

--- src/views/curso/Tema4.vue
+++ src/views/curso/Tema4.vue
@@ -68,7 +68,7 @@
       .row.justify-content-center.align-items-center
         .col-lg-1.col-6.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/30.svg', alt='')
         .col-lg-11(data-aos="fade-left")
-          p.mb-0.text-white Es importante destacar que la Comisión Interamericana de Derechos Humanos subraya que el enfoque de derechos humanos se sustenta en dos líneas fundamentales: 
+          p.mb-0.text-white Es importante destacar que la #[span.color-acento Comisión Interamericana de Derechos Humanos] subraya que el enfoque de derechos humanos se sustenta en dos líneas fundamentales: 
     .row.justify-content-center.mb-5
         .col-lg-6.mb-lg-0.mb-3(data-aos="fade-right")
           .d-flex.align-items-center.p-3.h-100(style="border-bottom: 1px solid #12263F")
@@ -174,8 +174,220 @@
           .text-white
             h4.mb-4 Calidad y calidez en la atención:
             p.mb-0 Proporcionar información de manera cálida y de calidad creará un ambiente de confianza y apertura, facilitando que las personas aclaren sus dudas y expresen necesidades particulares a su proceso. Por ejemplo, podría surgir una preocupación sobre la interacción entre los medicamentos prescritos y el tratamiento hormonal que una persona trans podría estar recibiendo, o la necesidad de apoyo psicosocial para la aceptación de diagnósticos que requieran ajustes significativos en su vida. Este último aspecto es crucial, especialmente si consideramos que algunas personas de los sectores sociales LGBTIQ+ pueden no contar con el apoyo de redes familiares o estar lejos de sus lugares de origen, lo cual dificulta la toma de decisiones y la adherencia a los tratamientos.
-    .p-4(style)
+    .p-4(style="background-color: #3c1c74 ")
+      p.mb-0.text-white #[b Ejemplo_] Enfoque de derechos en la atención de salud:
+        br
+        |El personal de salud debe garantizar una atención integral que incluya todos los elementos relevantes:
+    .p-5.bg-white.mb-5
+      ul.lista-ul--color
+        li.d-flex
+          i.fas.fa-heart
+          p.mb-0 #[b Anamnesis:] deben realizarse preguntas abiertas como "¿Por qué consulta hoy?", "¿Tiene alguna molestia?" o "¿En qué puedo ayudarle hoy?". Estas preguntas facilitan un buen inicio para un diálogo constructivo, permitiendo que personas LGBTIQ+ y con orientaciones sexuales e identidades de género no hegemónicas manifiesten sus inquietudes y necesidades.
+        li.d-flex
+          i.fas.fa-heart
+          p.mb-0 #[b Examen físico completo con énfasis en el órgano o sistema blanco:] realizar el examen físico habitual (inspección, palpación, percusión y auscultación) de acuerdo con las necesidades del paciente. Es fundamental que el examen sea pertinente y se maneje con cuidado, especialmente en aspectos sensibles relacionados con la identidad de la persona. Por ejemplo, en el caso de un hombre trans con tejido mamario, es pertinente realizar un examen de mama, explicando de manera respetuosa y clara el propósito del mismo, utilizando un lenguaje corporal y verbal adecuado (evitar expresiones de sorpresa o desagrado y usar términos neutros como "pecho" en lugar de "senos").
+      .ms-4.mb-5
+        ul.lista-ul--color
+          li.d-flex
+            i.fas.fa-circle.fa-xs(style="color: #0F5FFE ")
+            p.mb-0 Análisis de pruebas clínicas y paraclínicas: incluir exámenes de laboratorio, imágenes diagnósticas, electrocardiogramas, etc.
+          li.d-flex
+            i.fas.fa-circle.fa-xs(style="color: #0F5FFE ")
+            p.mb-0 Correlación entre pruebas para emitir diagnóstico.
+          li.d-flex
+            i.fas.fa-circle.fa-xs(style="color: #0F5FFE ")
+            p.mb-0 Formulación de tecnologías en salud y servicios complementarios requeridos por la persona.
+      p.mb-0 #[b Nota:] se debe activar la ruta integral de atención en salud dispuesta por el Ministerio de Salud y Protección Social, adecuada a las necesidades específicas de la persona. Por ejemplo, en el caso de un hombre trans con síntomas asociados a cáncer de cérvix, activar de inmediato la ruta de atención correspondiente, siempre utilizando un lenguaje cálido y afirmativo que respete su identidad.
+    .row.justify-content-center.mb-5
+      .col-lg-8.mb-lg-0.mb-3(data-aos="fade-right")
+        .tarjeta.p-4(style="background-color: #2c3e50 ").mb-5
+          .row.justify-content-center.align-items-center
+            .col-lg-1.col-6.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/57.svg', alt='')
+            .col-lg-11(data-aos="fade-left")
+              p.mb-0.text-white Es fundamental que el personal de salud incorpore en su práctica clínica acciones que promuevan el respeto por la dignidad humana y los derechos de todas las personas de los sectores LGBTIQ+ y con orientaciones sexuales e identidades de género no hegemónicas.
+        p.mb-5(data-aos='fade-right') Existen modelos de atención afirmativos y no afirmativos, los cuales se abordarán en profundidad en el componente 3. Para este componente, es crucial reconocer el concepto general de las acciones afirmativas, destinadas a #[b(style="color: #0F5FFE ") evitar la discriminación, marginación, exclusión y todo tipo de violencias que puedan afectar los derechos en salud de las personas que acuden al servicio.]
+          br
+          br
+          | #[span(style="color: #0F5FFE ") Como parte del enfoque de derechos humanos, se implementa el Enfoque de prevención, que tiene como objetivos: ]
+      .col-lg-4.col-7(data-aos="fade-left"): img(src='@/assets/curso/temas/58.png', alt='')
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .tarjeta-avatar-b.mb-3
+          .tarjeta-avatar-b__img(style="min-width: 14%")
+            img(src='@/assets/curso/temas/59.svg' alt='AvatarTop' style="width: 87px; left: 50px ")
+          .tarjeta(style="background-color: #88ebcb")
+            .p-4.ps-0
+              p.mb-0 Reducir o mitigar los efectos de la materialización de las violaciones de los derechos humanos.
+        .tarjeta-avatar-b.mb-3
+          .tarjeta-avatar-b__img(style="min-width: 14%")
+            img(src='@/assets/curso/temas/60.svg' alt='AvatarTop' style="width: 87px; left: 50px ")
+          .tarjeta(style="background-color: #88ebcb")
+            .p-4.ps-0
+              p.mb-0 Garantizar el cumplimiento de los derechos humanos.
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img(style="min-width: 14%")
+            img(src='@/assets/curso/temas/61.svg' alt='AvatarTop' style="width: 87px; left: 50px ")
+          .tarjeta(style="background-color: #88ebcb")
+            .p-4.ps-0
+              p.mb-0 Crear estrategias que prevengan la repetición de violaciones de los derechos humanos, incluyendo aquellas que puedan ocurrir en la prestación de los servicios de salud
+    .mn.py-lg-0(style="background-color: #3c1c74 ")
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-4.col-7.mb-lg-0.mb-3(data-aos="fade-right"): img(src='@/assets/curso/temas/62.svg', alt='')
+        .col-lg-8(data-aos="fade-left")
+          .p-5
+            p.mb-0.text-white #[span.color-acento Ejemplo_] El personal de salud debe ser capaz de identificar si una persona LGBTIQ+ o con orientaciones sexuales e identidades de género no hegemónicas, ha sido víctima de agresiones o violencia debido a su orientación sexual o identidad de género e indicar la ruta de activación dispuesta en la ciudad de residencia.
+    .row.justify-content-center.mb-3
+      .col-lg-5.mb-lg-0.mb-3
+        a.anexo.mb-4(:href="obtenerLink('downloads/prueba.pdf')" target="_blank")
+          .row.justify-content-around.align-items-center
+            .anexo__icono(style="width: 60px")
+              img(src="@/assets/template/icono-link.svg" )
+            .anexo__texto
+              p Enlace web. Para más información, se puede consultar el material disponible en la página web del Ministerio de Salud y Protección Social.
+      .col-lg-5.mb-lg-0.mb-3
+        a.anexo.mb-4(:href="obtenerLink('downloads/prueba.pdf')" target="_blank")
+          .row.justify-content-around.align-items-center
+            .anexo__icono(style="width: 60px")
+              img(src="@/assets/template/icono-pdf.svg" )
+            .anexo__texto
+              p Anexo. Cartilla Nada Justifica la violencia 
+    .row.justify-content-center.mb-5
+      .col-lg-5.mb-lg-0.mb-3
+        a.anexo.mb-4(:href="obtenerLink('downloads/prueba.pdf')" target="_blank")
+          .row.justify-content-around.align-items-center
+            .anexo__icono(style="width: 60px")
+              img(src="@/assets/curso/temas/icono-video.svg" )
+            .anexo__texto
+              p Video. Acto Vital
+      .col-lg-5.mb-lg-0.mb-3
+        a.anexo.mb-4(:href="obtenerLink('downloads/prueba.pdf')" target="_blank")
+          .row.justify-content-around.align-items-center
+            .anexo__icono(style="width: 60px")
+              img(src="@/assets/curso/temas/icono-video.svg" )
+            .anexo__texto
+              p Video. Ruta de atención a mujeres víctimas de violencia de género. Ley 1257/2008
+    p.mb-5(data-aos='fade-right') Tener en cuenta las orientaciones generales para el manejo y abordaje de violencia física y sexual:
+    .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+      h5 Figura 4. 
+      span Orientación para el manejo y abordaje de violencia física y sexual:
+    .mn.bg-4.mb-4
+      .row.justify-content-center.mb-5
+        .col-lg-8
+          img(src='@/assets/curso/temas/63.svg', alt='')
           
+    .row.justify-content-center.mb-5
+      .col-lg-8.mb-lg-0.mb-3(data-aos="fade-right")
+        .tarjeta.p-5(style="background-color: #2c3e50 ").mb-4
+          p.mb-0.text-white Durante la atención en salud, es crucial detectar todas las situaciones de violencia que puedan condicionar o perpetuar las afecciones por las cuales consultan las personas LGBTIQ+ y con orientaciones e identidades de género no hegemónicas. Es importante recordar que la violencia sexual también puede ocurrir en contextos de actividades sexuales remuneradas, lo cual debe ser atendido conforme a las rutas vigentes y con los enfoques pertinentes.
+        p.mb-3(data-aos='fade-right') Adicionalmente, se implementa el Enfoque de desarrollo humano, que garantiza la creación de escenarios culturales, sociales y económicos propicios para el respeto y la realización de los derechos de estos sectores sociales. Esto permite la potencialización de capacidades en lo social, económico y cultural, y está vinculado con el cambio de los imaginarios que forman parte de las experiencias cotidianas #[b(style="color: #0F5FFE ") (Ministerio del Interior, 2018). ]
+        .row.justify-content-center.align-items-center.mb-3
+          .col-lg-1.d-lg-block.d-none(data-aos="fade-right"): img(src='@/assets/curso/temas/dots.svg', alt='')
+          .col-lg-11
+            p.mb-0 Una manera de contribuir al Enfoque de desarrollo humano es a través del desarrollo social, ámbito en el que se pueden mejorar las condiciones de los servicios sociales básicos como la salud, la nutrición, la educación primaria y la planificación familiar.
+
+      .col-lg-4.col-7(data-aos="fade-left"): img(src='@/assets/curso/temas/64.png', alt='')
+    p.mb-5(data-aos='fade-right') #[b(style="color: #0F5FFE ") Ejemplo_] El personal de salud puede aportar al enfoque de desarrollo humano en la medida que genera recomendaciones en torno a los modos, condiciones y estilos de vida saludables. Puede basar las recomendaciones en su práctica y experiencia. Sin embargo, es importante tener en cuenta las recomendaciones dispuestas por el Ministerio de Salud y Protección Social respecto a:
+    .row.justify-content-center.mb-5
+      .col-lg-5.d-lg-block.d-none(data-aos="fade-right"): img(src='@/assets/curso/temas/65.png', alt='')
+      .col-lg-7(data-aos="fade-left")
+        .acordion-modificado 
+          AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--acc rounded-0")
+            div(titulo="Actividad física")
+              p.mb-0 Decálogo de la actividad física disponible en #[a(href="https://www.minsalud.gov.co/salud/publica/HS/Paginas/actividad-fisica.aspx"  target="_blank" ) https://www.minsalud.gov.co/salud/publica/HS/Paginas/actividad-fisica.aspx] Sugerir alternativas de actividad física que impliquen interacciones no sexistas, proponer roles equitativos en las actividades con todas las personas, hablar de referentes deportivos no hegemónicos e incluso sugerir la implementación de ejercicios que no refuercen estereotipos de masculinidad y femineidad.
+            div(titulo="Nutrición y alimentación saludable")
+              p.mb-0 Información sobre alimentación y nutrición, lactancia materna, control de deficiencia de micronutrientes, alimentación saludable, inocuidad y calidad de alimentos y política de seguridad alimentaria y nutricional en #[a(href="https://www.minsalud.gov.co/salud/publica/HS/Paginas/salud-nutricional.aspx"  target="_blank" ) https://www.minsalud.gov.co/salud/publica/HS/Paginas/salud-nutricional.aspx] 
+                br
+                |Fortalecer conceptos positivos sobre la imagen corporal para prevenir trastornos alimenticios. Evaluar calidad de dieta y riesgo de malnutrición.
+            div(titulo="Valoración del estado nutricional y estrategias")
+              p.mb-0 Detectar problemas y diagnósticos nutricionales integrales. Definir estrategias de intervención y seguimiento adecuado. Control por nutricionista acorde con necesidades puntuales identificadas.
+            div(titulo="Indicadores nutricionales para personas trans")
+              p.mb-0 La Academy of Nutrition and Dietetics recomienda el uso adecuado de indicadores nutricionales según la fase de transición. Los hombres Trans pueden tener demandas metabólicas aumentadas debido al uso de testosterona.
+            div(titulo="Prevención del consumo de tabaco")
+              p.mb-0 Prevención del consumo de tabaco y evaluación del uso de tabaco y otras sustancias en #[a(href="https://www.minsalud.gov.co/salud/publica/HS/Paginas/somos-mas-sin-tabaco.aspx"  target="_blank" ) https://www.minsalud.gov.co/salud/publica/HS/Paginas/somos-mas-sin-tabaco.aspx]
+                br
+                |Evaluar uso en los últimos 6 meses, 12 meses y 5 años.
+            div(titulo="Peso saludable")
+              p.mb-0 Consejos para mantener un peso saludable en #[a(href="https://www.minsalud.gov.co/salud/publica/HS/Paginas/peso-saludable.aspx"  target="_blank" ) https://www.minsalud.gov.co/salud/publica/HS/Paginas/peso-saludable.aspx]
+                br
+                |Evaluar déficit de vitaminas, calorías y micronutrientes en personas con dificultades en aumentar de peso o masa muscular, fatiga o ansiedad.
+            div(titulo="Lavado de manos")
+              p.mb-0 Importancia del lavado de manos detallada en #[a(href="https://www.minsalud.gov.co/salud/publica/HS/Paginas/lavado-de-manos.aspx"  target="_blank" ) https://www.minsalud.gov.co/salud/publica/HS/Paginas/lavado-de-manos.aspx]        
+            div(titulo="Salud bucal, visual y auditiva")
+              p.mb-0 Información sobre salud bucal, visual y auditiva en #[a(href="https://www.minsalud.gov.co/salud/publica/HS/Paginas/salud-bucal-visual-auditiva.aspx"  target="_blank" ) https://www.minsalud.gov.co/salud/publica/HS/Paginas/salud-bucal-visual-auditiva.aspx] 
+                br
+                |Hombres trans pueden tener sensibilidad, sangrado, inflamación y dolor en las encías por el uso de testosterona. Evaluar enfermedades en tejidos blandos y duros, hábitos relacionados con la salud bucal.
+    p.mb-5(data-aos='fade-right') #[b Enfoque de Orientaciones Sexuales e Identidades de Género Diversas.] Este enfoque que busca abordar y atender las diversas orientaciones sexuales e identidades de género, reconociendo y enfrentando la discriminación, marginación, exclusión y otras formas de violencia que afectan a las personas de los sectores LGBTIQ+. Se menciona en el Decreto 762/2018 en el contexto de la política pública nacional LGBTI.
+    .row.justify-content-center.mb-5
+      .col-lg-4.mb-lg-0.mb-3
+        .bg-white.tarjeta.overflow-hidden.sombra.h-100
+          img(src='@/assets/curso/temas/66.png', alt='')
+          .p-4
+            .text-center
+              h5.mb-4 Política Pública Nacional LGBTI
+              p.mb-0 Política que parte de reconocer la vulneración histórica de los derechos fundamentales de los sectores LGBTI, afirmando la igual dignidad y la condición inalienable de sus derechos. Promueve el restablecimiento de derechos y la consecución de la equidad bajo un enfoque diferencial.
+      .col-lg-4.mb-lg-0.mb-3
+        .bg-white.tarjeta.overflow-hidden.sombra.h-100
+          img(src='@/assets/curso/temas/67.png', alt='')
+          .p-4
+            .text-center
+              h5.mb-4 Condiciones diferenciales 
+              p.mb-0 Consideración explícita dentro del enfoque que aborda cómo las condiciones específicas de las personas intersexuales afectan el ejercicio efectivo de sus derechos, reconociendo sus luchas y demandas relacionadas con vulneraciones en los escenarios de atención de salud.
+      .col-lg-4.mb-lg-0.mb-3
+        .bg-white.tarjeta.overflow-hidden.sombra.h-100
+          img(src='@/assets/curso/temas/68.png', alt='')
+          .p-4
+            .text-center
+              h5.mb-4 Objetivo de atención en salud
+              p.mb-0 Relacionado con la promoción y garantía del ejercicio de los derechos económicos, sociales y culturales. Enfatiza en la igualdad de acceso y no discriminación en servicios como salud, educación, trabajo, vivienda, recreación, deporte y cultura para personas LGBTI, adoptando medidas técnicas y económicas necesarias.
+    img.mb-5(src='@/assets/curso/temas/18.svg', alt='')
+    p.mb-5(data-aos='fade-right') Desde este enfoque, el personal de salud debe desarrollar acciones y mecanismos afirmativos que garanticen una atención con equidad independiente de la Orientación Sexual e Identidad de Género. Para lo anterior, presentamos algunas acciones positivas para tener en cuenta: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </template>
 
